5edeced2d8e62ddb1fcdaa9cc8a5527fcbc58841
sandrajoseph111 <sandrajosephkodiyattu@gmail.com>
2026-07-13 10:38:59 +0530

    Register page added

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavigationBar from './NavigationBar'
+import axios from 'axios'
 
 const Add = () => {
+    
+    const [input, changeInput] = useState(
+
+
+        {
+
+            donor_name: "",
+             age: "",
+              gender: "",
+              blood_group: "",
+               phone:"",
+               email:"",
+               city:"",
+               weight_kg:"",
+               last_donation_date:""
+
+        }
+
+
+
+    )
+    const inputHandeler=(event)=>
+    {
+
+        changeInput({...input,[event.target.name]: event.target.value})
+
+    }
+
+    const readValue =()=>
+    {
+
+
+        console.log(input)
+        axios.post("https://host-demo-app.onrender.com/api/add-donor",input).then(
+
+            (response) =>{
+
+                console.log(response.data)
+                alert("donor added suceessfully")
+            }
+        ).catch(
+
+            (error)=>
+            {
+
+                console.log("Error adding donor",error)
+                alert("Failed to add donor")
+            }
+        )
+
+
+    }
   return (
     <div>
         <NavigationBar />
@@ -9,61 +62,66 @@ const Add = () => {
                 <div className="row">
                     <col className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" />
                     <div className="row g-3">
+
+
+
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Donor Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="donor_name" value={input.donor_name} onChange={inputHandeler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Age</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="age" value={input.age} onChange={inputHandeler} />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                             <label htmlFor="" className="form-label">Gender</label><br></br>
-                            <input type="radio" />MAle<br></br>
-                            <input type="radio" />Female
+                            <select name="" id="" className="form-control" name="gender" value={input.gender} onChange={inputHandeler}>
+                                <option value="Male">Male</option>
+                                <option value="Male">Female</option>
+                            </select>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Blood Group</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"name="blood_group" value={input.blood_group} onChange={inputHandeler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Phone</label>
-                            <input type="tel" className="form-control" />
+                            <input type="tel" className="form-control" name="phone" value={input.phone} onChange={inputHandeler} />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                             <label htmlFor="" className="form-label">Email</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="email" value={input.email} onChange={inputHandeler} />
                         </div>
                         
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">City</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control " name="city" value={input.city} onChange={inputHandeler}/>
                         </div>
 
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                             <label htmlFor="" className="form-label">Weight</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="weight_kg" value={input.weight_kg} onChange={inputHandeler} />
                         </div>
 
 
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                             <label htmlFor="" className="form-label">Last Donation Date</label>
-                            <input type="date" className="form-control" />
+                            <input type="date" className="form-control" name="last_donation_date" value={input.last_donation_date} onChange={inputHandeler} />
                         </div>
 
 
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                            <button className="btn btn-success">SUBMIT</button>
+                            <button className="btn btn-success" onClick={readValue}>SUBMIT</button>
                         </div>
                     </div>
 
@@ -76,5 +134,6 @@ const Add = () => {
     </div>
   )
 }
+
 
 export default Add
